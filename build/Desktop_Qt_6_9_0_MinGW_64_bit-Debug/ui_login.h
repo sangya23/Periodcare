3b80dbd8195105ea2558e05ab1a43c5eaebd13ad
sangya23 <88756433+sangya23@users.noreply.github.com>
2025-07-03 20:43:35 +0545

    moved files into folder

---
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/ui_login.h
@@ -0,0 +1,261 @@
+/********************************************************************************
+** Form generated from reading UI file 'login.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_LOGIN_H
+#define UI_LOGIN_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QFrame *frame;
+    QPushButton *lOGIN;
+    QLabel *label;
+    QLineEdit *usernamelineedit;
+    QLineEdit *pwdlineedit;
+    QLabel *label_2;
+    QPushButton *createaccount;
+    QPushButton *forgotpwd;
+    QStatusBar *statusbar;
+    QMenuBar *menubar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 600);
+        QPalette palette;
+        QLinearGradient gradient(0, 0, 1, 1);
+        gradient.setSpread(QGradient::Spread::PadSpread);
+        gradient.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush(gradient);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Button, brush);
+        QLinearGradient gradient1(0, 0, 1, 1);
+        gradient1.setSpread(QGradient::Spread::PadSpread);
+        gradient1.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient1.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient1.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush1(gradient1);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Base, brush1);
+        QLinearGradient gradient2(0, 0, 1, 1);
+        gradient2.setSpread(QGradient::Spread::PadSpread);
+        gradient2.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient2.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient2.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush2(gradient2);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Window, brush2);
+        QBrush brush3(QColor(255, 255, 255, 255));
+        brush3.setStyle(Qt::BrushStyle::SolidPattern);
+        palette.setBrush(QPalette::ColorGroup::Active, QPalette::ColorRole::Shadow, brush3);
+        QLinearGradient gradient3(0, 0, 1, 1);
+        gradient3.setSpread(QGradient::Spread::PadSpread);
+        gradient3.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient3.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient3.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush4(gradient3);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Button, brush4);
+        QLinearGradient gradient4(0, 0, 1, 1);
+        gradient4.setSpread(QGradient::Spread::PadSpread);
+        gradient4.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient4.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient4.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush5(gradient4);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Base, brush5);
+        QLinearGradient gradient5(0, 0, 1, 1);
+        gradient5.setSpread(QGradient::Spread::PadSpread);
+        gradient5.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient5.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient5.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush6(gradient5);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Window, brush6);
+        palette.setBrush(QPalette::ColorGroup::Inactive, QPalette::ColorRole::Shadow, brush3);
+        QLinearGradient gradient6(0, 0, 1, 1);
+        gradient6.setSpread(QGradient::Spread::PadSpread);
+        gradient6.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient6.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient6.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush7(gradient6);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Button, brush7);
+        QLinearGradient gradient7(0, 0, 1, 1);
+        gradient7.setSpread(QGradient::Spread::PadSpread);
+        gradient7.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient7.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient7.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush8(gradient7);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Base, brush8);
+        QLinearGradient gradient8(0, 0, 1, 1);
+        gradient8.setSpread(QGradient::Spread::PadSpread);
+        gradient8.setCoordinateMode(QGradient::CoordinateMode::ObjectBoundingMode);
+        gradient8.setColorAt(0, QColor(247, 198, 217, 255));
+        gradient8.setColorAt(1, QColor(230, 230, 250, 255));
+        QBrush brush9(gradient8);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Window, brush9);
+        palette.setBrush(QPalette::ColorGroup::Disabled, QPalette::ColorRole::Shadow, brush3);
+        MainWindow->setPalette(palette);
+        MainWindow->setStyleSheet(QString::fromUtf8("QWidget {\n"
+"    background: qlineargradient(\n"
+"        x1: 0, y1: 0, x2: 1, y2: 1,\n"
+"       stop: 0 #f7c6d9,  \n"
+"       stop: 1 #e6e6fa\n"
+"    );\n"
+"}\n"
+"\n"
+""));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        frame = new QFrame(centralwidget);
+        frame->setObjectName("frame");
+        frame->setGeometry(QRect(70, 50, 671, 421));
+        frame->setStyleSheet(QString::fromUtf8("QFrame {\n"
+"    background-color: #fce4ec;\n"
+"    border: 2px solid #F497B6 ;\n"
+"    border-radius: 20px;\n"
+"}"));
+        frame->setFrameShape(QFrame::Shape::StyledPanel);
+        frame->setFrameShadow(QFrame::Shadow::Raised);
+        lOGIN = new QPushButton(frame);
+        lOGIN->setObjectName("lOGIN");
+        lOGIN->setGeometry(QRect(110, 240, 91, 31));
+        lOGIN->setAutoFillBackground(false);
+        lOGIN->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"font-size:12px;\n"
+"}\n"
+"QPushButton {\n"
+"    background-color: #F497B6;      \n"
+"    color: white;                   \n"
+"    font: bold 14px \"Arial\";\n"
+"    border-radius: 8px;\n"
+"    padding: 6px 12px;\n"
+"}\n"
+""));
+        label = new QLabel(frame);
+        label->setObjectName("label");
+        label->setGeometry(QRect(90, 40, 101, 41));
+        label->setStyleSheet(QString::fromUtf8("QLabel{\n"
+"    background-color: #F497B6;      \n"
+"    color: #3D3D3D;                   \n"
+"    font: bold 20px \"Arial\";\n"
+"    border-radius: 8px;\n"
+"    padding: 6px 12px;\n"
+"}"));
+        usernamelineedit = new QLineEdit(frame);
+        usernamelineedit->setObjectName("usernamelineedit");
+        usernamelineedit->setGeometry(QRect(20, 130, 261, 31));
+        usernamelineedit->setStyleSheet(QString::fromUtf8("QLineEdit {\n"
+"    background-color: #FFFFFF;\n"
+"    color: #3D3D3D;\n"
+"    font: 13px \"Segoe UI\";\n"
+"    border: 1px solid #CCCCCC;\n"
+"    border-radius: 6px;\n"
+"    padding: 6px;\n"
+"}"));
+        pwdlineedit = new QLineEdit(frame);
+        pwdlineedit->setObjectName("pwdlineedit");
+        pwdlineedit->setGeometry(QRect(20, 170, 261, 31));
+        pwdlineedit->setStyleSheet(QString::fromUtf8("QLineEdit {\n"
+"    background-color: #FFFFFF;\n"
+"    color: #3D3D3D;\n"
+"    font: 13px \"Segoe UI\";\n"
+"    border: 1px solid #CCCCCC;\n"
+"    border-radius: 6px;\n"
+"    padding: 6px;\n"
+"}"));
+        label_2 = new QLabel(frame);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(310, 0, 361, 421));
+        label_2->setStyleSheet(QString::fromUtf8("QLabel\n"
+"{\n"
+"	background-color:#f7cfd0;\n"
+"}"));
+        label_2->setPixmap(QPixmap(QString::fromUtf8(":/loginicons/periodsimg1.jpg")));
+        label_2->setScaledContents(true);
+        label_2->setMargin(5);
+        createaccount = new QPushButton(frame);
+        createaccount->setObjectName("createaccount");
+        createaccount->setGeometry(QRect(160, 290, 131, 26));
+        createaccount->setAutoFillBackground(false);
+        createaccount->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"font-size:12px;\n"
+"}\n"
+"QPushButton {\n"
+"    background-color: #F497B6;      \n"
+"    color: white;                   \n"
+"    font: 12px \"Arial\";\n"
+"    border-radius: 8px;\n"
+"    padding: 6px 12px;\n"
+"}\n"
+""));
+        forgotpwd = new QPushButton(frame);
+        forgotpwd->setObjectName("forgotpwd");
+        forgotpwd->setGeometry(QRect(10, 290, 141, 26));
+        forgotpwd->setAutoFillBackground(false);
+        forgotpwd->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #F497B6;      \n"
+"    color: white;                   \n"
+"    font: 12px \"Arial\";\n"
+"    border-radius: 8px;\n"
+"    padding: 6px 12px;\n"
+"}\n"
+""));
+        label_2->raise();
+        lOGIN->raise();
+        label->raise();
+        usernamelineedit->raise();
+        pwdlineedit->raise();
+        createaccount->raise();
+        forgotpwd->raise();
+        MainWindow->setCentralWidget(centralwidget);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 18));
+        MainWindow->setMenuBar(menubar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        lOGIN->setText(QCoreApplication::translate("MainWindow", "Login", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p align=\"center\">Login</p></body></html>", nullptr));
+        usernamelineedit->setText(QString());
+        pwdlineedit->setText(QString());
+        label_2->setText(QString());
+        createaccount->setText(QCoreApplication::translate("MainWindow", "Create an account", nullptr));
+        forgotpwd->setText(QCoreApplication::translate("MainWindow", "Forgot Password?", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_LOGIN_H
