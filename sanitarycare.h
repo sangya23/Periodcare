4d3c137d1b71be8e722390e785ef141c001bc4f2
Pragyee2024 <pragyee2007@gmail.com>
2025-07-20 09:22:15 +0545

    adding sanitary care

---
+++ sanitarycare.h
@@ -0,0 +1,23 @@
+#ifndef SANITARYCARE_H
+#define SANITARYCARE_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class SanitaryCare;
+}
+QT_END_NAMESPACE
+
+class SanitaryCare : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    SanitaryCare(QWidget *parent = nullptr);
+    ~SanitaryCare();
+
+private:
+    Ui::SanitaryCare *ui;
+};
+#endif // SANITARYCARE_H
